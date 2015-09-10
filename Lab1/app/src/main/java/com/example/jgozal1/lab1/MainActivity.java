a17ec2556b1285d16f5da61574e2ef0a0fab63fc
Jimmy Gozal <jgozal@developerkit.co>
2015-09-10 19:24:52 -0400

    working

--- Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.jgozal1.lab1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +33,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_item:
-                Log.d("MainActivity","Add a new item");
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add a item");
+                builder.setMessage("What do you want to put in your shopping list?");
+                final EditText inputField = new EditText(this);
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("MainActivity", inputField.getText().toString());
+                    }
+                });
+
+                builder.setNegativeButton("Cancel",null);
+
+                builder.create().show();
                 return true;
+
+// ... other code
 
             default:
                 return false;
