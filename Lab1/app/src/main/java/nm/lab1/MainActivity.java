90e755ad82560da72cbcfae19b8026574b2a0bb7
Nora Mohamed <jurukix@gmail.com>
2015-09-11 08:49:35 -0400

    basic functionality
    you can add stuff to a list with a button and do nothing else, hooray

--- Lab1/app/src/main/java/nm/lab1/MainActivity.java
+++ Lab1/app/src/main/java/nm/lab1/MainActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        final EditText textbox = (EditText) findViewById(R.id.editText);
+        ListView list = (ListView) findViewById(R.id.list);
+
+        final ArrayList<String> arrayList = new ArrayList<String>();
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(listAdapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = textbox.getText().toString();
+                if(null != input && input.length() > 0){
+                    arrayList.add(input);
+                    listAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
 
