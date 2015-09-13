e414bc12acc03aa785267ecd4030decec6a47bfa
David Abrahams <david.abrahams@students.olin.edu>
2015-09-13 18:10:48 -0400

    Implemented new item adding!

--- GroceryList/app/src/main/java/com/mobileproto/david/grocerylist/MainActivity.java
+++ GroceryList/app/src/main/java/com/mobileproto/david/grocerylist/MainActivity.java
@@ -9,36 +9,56 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
 
     private static String logTag = "GroceryApp";
     private ListView listView;
+    private ArrayList<String> strings;
+    private ArrayAdapter<String> itemsAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] strings = {"Pizza", "Pasta", "Gainz"};
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
+
+        this.strings = new ArrayList<>();
+        this.itemsAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, strings);
         this.listView = (ListView) findViewById(R.id.list_view);
         this.listView.setAdapter(itemsAdapter);
-        Button newItemButton = (Button) findViewById(R.id.button_new_item);
 
-        newItemButton.setOnClickListener(new View.OnClickListener() {
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+
+            }
+        });
+
+        Button newItemButton = (Button) findViewById(R.id.button_new_item);
+        newItemButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
                 promptNewItem();
             }
         });
     }
 
-    private void promptNewItem() {
+    private void promptNewItem()
+    {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.dialog_title);
 
@@ -46,31 +66,43 @@ public class MainActivity extends AppCompatActivity {
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         builder.setView(input);
 
-        builder.setPositiveButton(R.string.dialog_confirm, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.dialog_confirm, new DialogInterface.OnClickListener()
+        {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialog, int which)
+            {
                 String text = input.getText().toString();
-                Log.d(logTag, text);
+                addItem(text);
             }
         });
-        builder.setNegativeButton(R.string.dialog_deny, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(R.string.dialog_deny, new DialogInterface.OnClickListener()
+        {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialog, int which)
+            {
                 dialog.cancel();
             }
         });
         builder.show();
     }
 
+    private void addItem(String item)
+    {
+        strings.add(item);
+        itemsAdapter.notifyDataSetChanged();
+    }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
