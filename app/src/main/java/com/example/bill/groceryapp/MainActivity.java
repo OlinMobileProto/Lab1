b2c9c0c60a0f49c053125790defb01eec2421a67
Bill Wong <billmwong@gmail.com>
2015-09-15 15:51:32 -0400

    Can add, edit, and delete items

--- app/src/main/java/com/example/bill/groceryapp/MainActivity.java
+++ app/src/main/java/com/example/bill/groceryapp/MainActivity.java
@@ -1,16 +1,72 @@
 package com.example.bill.groceryapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    private EditText newItemEditText;
+    private ArrayList<String> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        final ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+        myButton = (Button) findViewById(R.id.button);
+        newItemEditText = (EditText) findViewById(R.id.editText);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                myButton.setText("Hello");
+                items.add(newItemEditText.getText().toString());
+                newItemEditText.setText("");
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final EditText input = new EditText(MainActivity.this);
+                input.setText(parent.getItemAtPosition(position).toString());
+                builder.setTitle("Edit Item");
+                builder.setPositiveButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        items.set(position, input.getText().toString());
+                        itemsAdapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNeutralButton("Cancel", null);
+                builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        items.remove(position);
+                        itemsAdapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setView(input);
+                builder.show();
+            }
+        });
     }
 
     @Override
