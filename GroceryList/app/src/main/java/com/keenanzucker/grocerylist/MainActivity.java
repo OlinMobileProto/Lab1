278f9224f6645e6d4fdc0215d9a9440f378c597d
Keenan Zucker <Keenan.Zucker@students.olin.edu>
2015-09-15 17:22:55 -0400

    Added editing feature to notes

--- GroceryList/app/src/main/java/com/keenanzucker/grocerylist/MainActivity.java
+++ GroceryList/app/src/main/java/com/keenanzucker/grocerylist/MainActivity.java
@@ -1,7 +1,11 @@
 package com.keenanzucker.grocerylist;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
+    final Context context = this;
+    private String inputText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +32,76 @@ public class MainActivity extends AppCompatActivity {
 
 
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
-        itemsAdapter = new ArrayAdapter<String>(this,
+
+        items = new ArrayList<>();
+
+        //Use ArrayAdapter to create scrollable list
+        itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
+
+        //Example note to display
+        items.add("Example Note");
         setupListViewListener();
 
     }
 
+    //Edit or remove notes by listening to the user clicks
     private void setupListViewListener() {
+
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+
+                    //Single click on list item to edit
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter,
+                                                View item, int pos, long id)
+                    {
+                        //Edit text through an Alert Dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                        builder.setTitle("Edit Note");
+
+                        final EditText input = new EditText(context);
+                        final int itemPos = pos;
+
+                        input.setInputType(InputType.TYPE_CLASS_TEXT);
+                        builder.setView(input);
+
+                        //Positive button on dialog box -- "change"
+                        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                inputText = input.getText().toString();
+
+                                // Remove the item within array at position
+                                items.remove(itemPos);
+                                // Then insert item at same location -- aka 'editing' the text
+                                itemsAdapter.insert(inputText, itemPos);
+                                // Refresh the adapter
+                                itemsAdapter.notifyDataSetChanged();
+
+                            }
+                        });
+
+                        //Negative button on dialog box -- "cancel"
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+
+                        });
+
+                        //Show the dialog box!
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                }
+        );
+
+        // Long click list items to delete them
         lvItems.setOnItemLongClickListener(
                 new AdapterView.OnItemLongClickListener() {
                     @Override
@@ -47,10 +114,11 @@ public class MainActivity extends AppCompatActivity {
                         // Return true consumes the long click event (marks it handled)
                         return true;
                     }
-
-                });
+                }
+        );
     }
 
+    // Add the items when the button is clicked!
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
