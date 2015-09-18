6f2b67c345d1386976d7d84444264a8e01683e2c
Jimmy Gozal <jgozal@developerkit.co>
2015-09-18 15:54:20 -0400

    working interface, no database yet

--- Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
@@ -2,29 +2,25 @@ package com.example.jgozal1.lab1;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     /**
      * Called when the activity is first created.
      */
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
     }
@@ -34,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_add_item:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Add a item");
-                builder.setMessage("What do you want to put in your shopping list?");
+                builder.setTitle("Add an item");
+                builder.setMessage("What do you need to buy?");
                 final EditText inputField = new EditText(this);
                 builder.setView(inputField);
                 builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("MainActivity", inputField.getText().toString());
+                        Log.d("MainActivity",inputField.getText().toString());
                     }
                 });
 
@@ -56,5 +52,4 @@ public class MainActivity extends AppCompatActivity {
                 return false;
         }
     }
-
 }
