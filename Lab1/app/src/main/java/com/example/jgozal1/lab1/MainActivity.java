94f4ff20cac22930c2cd69c30ced9adbed6f0ec5
Jimmy Gozal <jgozal@developerkit.co>
2015-09-19 02:33:45 -0400

    database in, app not functional

--- Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/jgozal1/lab1/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.jgozal1.lab1;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    /**
-     * Called when the activity is first created.
-     */
+        import android.app.AlertDialog;
+        import android.app.ListActivity;
+        import android.content.ContentValues;
+        import android.content.DialogInterface;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.*;
+        import com.example.jgozal1.lab1.db.ItemContract;
+        import com.example.jgozal1.lab1.db.ItemDBHelper;
+
+public class MainActivity extends ListActivity {
+    private ListAdapter listAdapter;
+    private ItemDBHelper helper;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        updateUI();
     }
 
     @Override
@@ -31,13 +39,23 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_add_item:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Add an item");
-                builder.setMessage("What do you need to buy?");
+                builder.setMessage("What would you like to buy?");
                 final EditText inputField = new EditText(this);
                 builder.setView(inputField);
                 builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("MainActivity",inputField.getText().toString());
+                        String item = inputField.getText().toString();
+
+                        helper = new ItemDBHelper(MainActivity.this);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+
+                        values.clear();
+                        values.put(ItemContract.Columns.ITEM,item);
+
+                        db.insertWithOnConflict(ItemContract.TABLE,null,values,SQLiteDatabase.CONFLICT_IGNORE);
+                        updateUI();
                     }
                 });
 
@@ -46,10 +64,44 @@ public class MainActivity extends AppCompatActivity {
                 builder.create().show();
                 return true;
 
-// ... other code
-
             default:
                 return false;
         }
+    }
+
+    private void updateUI() {
+        helper = new ItemDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(ItemContract.TABLE,
+                new String[]{ItemContract.Columns._ID, ItemContract.Columns.ITEM},
+                null, null, null, null, null);
+
+        listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.item_view,
+                cursor,
+                new String[]{ItemContract.Columns.ITEM},
+                new int[]{R.id.itemTextView},
+                0
+        );
+
+        this.setListAdapter(listAdapter);
+    }
+
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView itemTextView = (TextView) v.findViewById(R.id.itemTextView);
+        String item = itemTextView.getText().toString();
+
+        String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
+                ItemContract.TABLE,
+                ItemContract.Columns.ITEM,
+                item);
+
+
+        helper = new ItemDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getWritableDatabase();
+        sqlDB.execSQL(sql);
+        updateUI();
     }
 }
